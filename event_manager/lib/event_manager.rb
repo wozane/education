4a0e03c013caa361cb8b45021c4f759546deea91
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-05-30 15:15:59 +0200

    Load and display first_name from event_attendees.csv

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,1 +1,9 @@
 puts "EventManager Initialized!"
+
+lines = File.readlines "event_attendees.csv"
+lines.each_with_index do |line, index|
+  next if index == 0
+  columns = line.split(",")
+  name = columns[2]
+  puts name
+end
