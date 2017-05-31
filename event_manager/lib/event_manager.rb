9c440ce29d61ca259747ab3d00fc287b6e744335
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-05-31 11:13:31 +0200

    Add the sunlight gem and display legistators

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,9 +1,9 @@
 require "csv"
-
 require "sunlight/congress"
-require "erb"
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+require "erb"
 
 def legislators_by_zipcode(zipcode)
   Sunlight::Congress::Legislator.by_zipcode(zipcode)
@@ -14,10 +14,17 @@ def save_thank_you_letters(id,form_letter)
 
   filename = "output/thanks_#{id}.html"
 
-  File.open(filename,'w') do |file|
-    file.puts form_letter
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+  legislator_names = legislators.collect do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
   end
+
+  legislator_names.join(", ")
 end
+
+puts "EventManager Initialized!"
 
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
@@ -32,4 +39,7 @@ contents.each do |row|
   form_letter = erb_template.result(binding)
 
   save_thank_you_letters(id,form_letter)
+
+  puts "#{name} #{zipcode} #{legislators}"
+
 end
