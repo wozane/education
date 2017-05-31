689249adc00fd48b58f72c44d09ebfb2366ba3f2
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-05-31 10:35:46 +0200

    Add clean_zipcode method

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,12 +1,9 @@
 require "csv"
+
 require "sunlight/congress"
 require "erb"
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
-
-def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5, "0")[0..4]
-end
 
 def legislators_by_zipcode(zipcode)
   Sunlight::Congress::Legislator.by_zipcode(zipcode)
@@ -22,16 +19,13 @@ def save_thank_you_letters(id,form_letter)
   end
 end
 
-puts "EventManager Initialized!"
-
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
-
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
 
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
+
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
