b49dfe76b2b155f5bf3432204e2789de1c9ffc97
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-06-01 15:25:06 +0200

    Refactor test for leap_year as per review

--- day_adding/test_leap_year.rb
+++ day_adding/test_leap_year.rb
@@ -8,18 +8,18 @@ require_relative 'calendar.rb'
 
 class TestLeapYear < Minitest::Test
   def test_if_2016_is_leap_year
-    assert_equal(true, year_leap?(2016))
+    assert year_leap?(2016)
   end
 
   def test_that_1900_is_not_eap_year
-    assert_equal(false, year_leap?(1900))
+    assert !year_leap?(1900)
   end
 
   def test_that_1999_is_not_eap_year
-    assert_equal(false, year_leap?(1999))
+    assert !year_leap?(1999)
   end
 
   def test_that_1600_is_leap_year
-   assert_equal(true, year_leap?(1600))
+   assert year_leap?(1600)
  end 
 end
