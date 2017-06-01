968aab39d51cfa4425cec436f68800ed85323c49
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-06-01 15:12:34 +0200

    Creat test file for leap_year method

---
+++ day_adding/test_leap_year.rb
@@ -0,0 +1,21 @@
+require 'simplecov'
+SimpleCov.start 'rails'
+
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative 'calendar.rb'
+
+class TestLeapYear < Minitest::Test
+  def test_if_2016_is_leap_year
+    assert_equal(true, year_leap?(2016))
+  end
+
+  def test_that_1900_is_not_eap_year
+    assert_equal(false, year_leap?(1900))
+  end
+
+  def test_that_1999_is_not_eap_year
+    assert_equal(false, year_leap?(1999))
+  end  
+end
