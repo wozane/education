e5e7cfba0217753d3555536c192ad43dadb4a4bd
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-06-05 09:12:17 +0200

    Add tests for validation of phone number
    Change the validate method

--- event_manager/test/test_validate_phone_number.rb
+++ event_manager/test/test_validate_phone_number.rb
@@ -6,28 +6,24 @@ require 'minitest/pride'
 require_relative '../lib/validate_phone_number.rb'
 
 class ValidatePhoneTest < Minitest::Test
-  def setup
-    @phone_number = ValidatePhoneNumber.new    
-  end
 
   def test_that_the_code_will_not_accept_invalid_format
-    assert_equal("Invalid phone format", @phone_number.call(nil))  
+    assert_equal('Invalid phone number length', ValidatePhoneNumber.validate(nil))
   end
 
   def test_the_number_of_digits
-    assert_equal("Invalid phone number length", @phone_number.call("123456"))
+    assert_equal('Invalid phone number length', ValidatePhoneNumber.validate('123456'))
   end
 
   def test_the_code_return_correct_number
-    assert_equal("1234567890", @phone_number.call("1234567890"))
-  end
-
-  def test_that_the_code_will_not_accept_numbers_longer_than_11
-    assert_equal("Invalid phone number length", @phone_number.call("12345678901"))
+    assert_equal('1234567890', ValidatePhoneNumber.validate('1234567890'))
   end
 
   def test_if_the_number_is_11_digit_long_and_if_the_first_digit_is_1_then_trim
-    
+    assert_equal('2345678901', ValidatePhoneNumber.validate('12345678901'))
   end
 
+  def test_will_not_accept_numbers_longer_than_11_and_first_digit_not_1
+    assert_equal('Invalid phone number', ValidatePhoneNumber.validate('23456789012'))
+  end
 end
