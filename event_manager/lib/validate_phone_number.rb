4ecaf7fdb08419909ec228c7ad125f17eaac5215
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-06-05 09:09:38 +0200

    Set code and tests for validation of phone number

--- event_manager/lib/validate_phone_number.rb
+++ event_manager/lib/validate_phone_number.rb
@@ -1,12 +1,18 @@
 class ValidatePhoneNumber
   def call(number)
-    return "Invalid phone format" unless number.kind_of?(String)
+    return 'Invalid phone format' unless number.is_a?(String)
 
     case number.length
     when 10
       number
+    when 11
+      if number[0] == '1'
+        number[1..-1]
+      else
+        'Invalid phone number'
+      end
     else
-      "Invalid phone number length"
+      'Invalid phone number length'
     end
   end
 end
