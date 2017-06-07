95b1570b15c95eec286fed30e8d44e6a549854f4
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-06-07 11:36:19 +0200

    Update the code as per tutorial

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,8 +1,12 @@
-require "csv"
-require "sunlight/congress"
-require "erb"
+require 'csv'
+require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
 
 def legislators_by_zipcode(zipcode)
   Sunlight::Congress::Legislator.by_zipcode(zipcode)
@@ -18,7 +22,9 @@ def save_thank_you_letters(id,form_letter)
   end
 end
 
-puts "EventManager Initialized!"
+puts "EventManager initialized."
+
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
 
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
@@ -32,6 +38,4 @@ contents.each do |row|
   form_letter = erb_template.result(binding)
 
   save_thank_you_letters(id,form_letter)
-
-  puts "#{name} #{zipcode} #{legislators}"
 end
