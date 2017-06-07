336aa14a3530e523d9c66fdddcfc719c27bdeafe
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-06-07 10:18:09 +0200

    Apply changes as per code review

--- event_manager/lib/validate_phone_number.rb
+++ event_manager/lib/validate_phone_number.rb
@@ -1,18 +1,14 @@
 class ValidatePhoneNumber
-  def self.validate(number)
+  def self.validate!(number)
     raise ArgumentError.new('Invalid phone format') unless number.is_a?(String)
+    raise ArgumentError.new('Invalid phone number length') if number.length < 10 || number.length > 11
 
     case number.to_s.length
     when 10
       number
     when 11
-      if number[0] == '1'
-        number[1..-1]
-      else
-        'Invalid phone number'
-      end
-    else
-      'Invalid phone number length'
+      raise ArgumentError.new('Invalid phone number') if number[0] != '1'
+      number[1..-1]
     end
   end
 end
--- event_manager/test/test_validate_phone_number.rb
+++ event_manager/test/test_validate_phone_number.rb
@@ -6,25 +6,26 @@ require 'minitest/pride'
 require_relative '../lib/validate_phone_number.rb'
 
 class ValidatePhoneTest < Minitest::Test
-
   def test_the_number_of_digits
-    assert_equal('Invalid phone number length', ValidatePhoneNumber.validate('123456'))
+    exception = assert_raises(ArgumentError) { ValidatePhoneNumber.validate!('123456') }
+    assert_equal(exception.message, 'Invalid phone number length')
   end
 
   def test_that_the_code_will_not_accept_invalid_format
-    exception = assert_raises(ArgumentError) { ValidatePhoneNumber.validate(nil) }
+    exception = assert_raises(ArgumentError) { ValidatePhoneNumber.validate!(nil) }
     assert_equal(exception.message, 'Invalid phone format')
   end
 
   def test_the_code_return_correct_number
-    assert_equal('1234567890', ValidatePhoneNumber.validate('1234567890'))
+    assert_equal('1234567890', ValidatePhoneNumber.validate!('1234567890'))
   end
 
   def test_if_the_number_is_11_digit_long_and_if_the_first_digit_is_1_then_trim
-    assert_equal('2345678901', ValidatePhoneNumber.validate('12345678901'))
+    assert_equal('2345678901', ValidatePhoneNumber.validate!('12345678901'))
   end
 
   def test_will_not_accept_numbers_longer_than_11_and_first_digit_not_1
-    assert_equal('Invalid phone number', ValidatePhoneNumber.validate('23456789012'))
+    exception = assert_raises(ArgumentError) { ValidatePhoneNumber.validate!('23456789012') }
+    assert_equal(exception.message, 'Invalid phone number')
   end
 end
