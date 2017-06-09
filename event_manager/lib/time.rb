76cfc6532445d0478bff620b8437c08c32b7b873
Aneta Wozniak <wozniakaneta5@gmail.com>
2017-06-09 16:23:48 +0200

    Set code to count hours

--- event_manager/lib/time.rb
+++ event_manager/lib/time.rb
@@ -1,8 +1,36 @@
 require 'csv'
 
-contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
-contents.each do |row|
-  time = DateTime.strptime(row[1], "%m/%d/%y %H:%M")
+class StripTime
+  attr_accessor :hours
 
-  puts "#{ time.hour }"
+  def initialize
+    @hours = []
+  end
+
+  def call
+    get_hours_from_csv
+    calcualte_hours
+  end
+
+  private
+  def get_hours_from_csv
+    CSV.foreach('event_attendees.csv', headers: true) do |row|
+      hours << convert_date(row[1])
+    end
+  end
+
+  def calcualte_hours
+    hours.inject({}) do |total, hour|
+      total[hour] ||= 0
+      total[hour] += 1
+      total
+    end
+  end
+
+  def convert_date(csv_date)
+    DateTime.strptime(csv_date, "%m/%d/%y %H:%M").hour
+  end
 end
+
+processor = StripTime.new
+puts "Most hours: #{processor.call} "
--- event_manager/test/test_time.rb
+++ event_manager/test/test_time.rb
@@ -8,5 +8,8 @@ SimpleCov.start 'rails'
 require_relative '../lib/time.rb'
 
 class TimeTargetingTest < Minitest::Test
-
+  def test_which_hours_of_the_day_most_ppl_registered
+    ary = [1, 2, 3, 3]
+    assert_equal(3, ary.most_hours)
+  end
 end
